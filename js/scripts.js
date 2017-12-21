ab1f5a93e12c1c57ee0c450cab0f244be4775f44
Arnold Sztandarski <a.sztandarski@wp.pl>
2017-12-21 18:20:05 +0100

    cd

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-	$(function changeSlide() {
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-});
-$(setInterval(changeSlide, 3000));
+
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	};
+
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+	setInterval(changeSlide, 3000);
 });
 
 
